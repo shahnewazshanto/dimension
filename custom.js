2aaaf968a05f1be8a9656e9a0d30f78288eb7a8d
tamjidashraf <tamjidmd50@gmail.com>
2022-09-10 17:17:58 +0600

    version1

---
+++ custom.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.courses', {
+  // Waits 1000ms after typing "First"
+  strings: ['Graphic Design', 
+  'Web Design', 
+  'Web Development', 
+  'Digital Marketing'],
+  typeSpeed: 100,
+  loop: true,
+  cursorChar: '👈'
+});
